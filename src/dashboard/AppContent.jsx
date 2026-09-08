aba8e8794cf8c5b1a3218b5b2c84e49602f5e4d5
mencariberkah <mencariberkah807@gmail.com>
2026-09-08 11:21:03 +0700

    Pass eclipse detail action into natural layer

--- src/dashboard/AppContent.jsx
+++ src/dashboard/AppContent.jsx
@@ -13,12 +13,13 @@ export default function AppContent({
   onJumpToday,
   quickJumps,
   onOpenWeton,
+  onOpenEclipse,
 }) {
   return (
     <div className="w-full px-4 py-6 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
         <div className="flex flex-col gap-6 lg:col-span-8">
-          <NaturalLayer data={data} loading={false} />
+          <NaturalLayer data={data} loading={false} onOpenEclipse={onOpenEclipse} />
           <SunArc sun={data.natural.sun} time={time} loading={false} />
           <CalendarSystems data={data} loading={false} onOpenWeton={onOpenWeton} />
         </div>
