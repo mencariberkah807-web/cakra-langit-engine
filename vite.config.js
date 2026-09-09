c76c448fe6a2e38565fd0f4fc094c50634c51bab
mencariberkah <mencariberkah807@gmail.com>
2026-09-09 19:08:25 +0700

    Use Vite proxy for local API in Codespaces

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
