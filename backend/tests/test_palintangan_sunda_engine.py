3f4e7c88ff3addbea8cd664ea94d20d6d9d7daa9
mencariberkah <mencariberkah807@gmail.com>
2026-09-26 16:53:23 +0700

    expand palintangan regression coverage

--- backend/tests/test_palintangan_sunda_engine.py
+++ backend/tests/test_palintangan_sunda_engine.py
@@ -1,6 +1,16 @@
 import unittest
 
-from engines.palintangan_sunda_engine import calculate_jaya_apes
+from engines.palintangan_sunda_engine import (
+    DINO_NAKTU,
+    GAGALANG_PASARAN,
+    GAGALANG_POE_MONTHS,
+    MONTH_RULES,
+    PASARAN_NAKTU,
+    PERNAASAN,
+    WATEK_PATOKAN,
+    calculate_jaya_apes,
+    normalize_hijri_month,
+)
 
 
 class PalintanganJayaApesTests(unittest.TestCase):
@@ -17,6 +27,37 @@ class PalintanganJayaApesTests(unittest.TestCase):
         values = [calculate_jaya_apes("Senen", n)["jaya_index"] for n in range(7, 21)]
         self.assertEqual(values[:7], [0, 1, 2, 3, 4, 5, 6])
         self.assertEqual(values[7:], [0, 1, 2, 3, 4, 5, 6, 0])
+
+    def test_locked_naktu_tables(self):
+        self.assertEqual(DINO_NAKTU["Senen"], 4)
+        self.assertEqual(DINO_NAKTU["Kemis"], 8)
+        self.assertEqual(DINO_NAKTU["Ngahad"], 5)
+        self.assertEqual(PASARAN_NAKTU["Pahing"], 9)
+        self.assertEqual(PASARAN_NAKTU["Wage"], 4)
+        self.assertEqual(PASARAN_NAKTU["Kliwon"], 8)
+
+    def test_locked_watek_and_gagalang_tables(self):
+        self.assertEqual(len(WATEK_PATOKAN), 12)
+        self.assertEqual(WATEK_PATOKAN["Muharam"]["watek"], "Wani")
+        self.assertEqual(WATEK_PATOKAN["Rayagung"]["watek"], "Alas Kobar")
+        self.assertEqual(GAGALANG_POE_MONTHS["Puasa"]["hari"], "Rebo")
+        self.assertEqual(GAGALANG_POE_MONTHS["Puasa"]["watek"], "Demang Kanduruan")
+        self.assertEqual(GAGALANG_PASARAN["Pahing"]["direction"], "Barat")
+
+    def test_locked_pernaasan_and_navigation_groups(self):
+        self.assertEqual(PERNAASAN["Muharam"], [3, 12, 20])
+        self.assertEqual(PERNAASAN["Puasa"], [9, 20, 29])
+        self.assertEqual(PERNAASAN["Sawal"], [])
+        self.assertEqual(MONTH_RULES["Muharam"]["rizki_direction"], "Tenggara")
+        self.assertEqual(MONTH_RULES["Rabiulakhir"]["rizki_direction"], "Barat Laut")
+        self.assertEqual(MONTH_RULES["Rajab"]["rizki_direction"], "Barat Daya")
+        self.assertEqual(MONTH_RULES["Sawal"]["rizki_direction"], "Timur Laut")
+
+    def test_hijri_aliases_normalize_to_sunda_months(self):
+        self.assertEqual(normalize_hijri_month("Muharram"), "Muharam")
+        self.assertEqual(normalize_hijri_month("Sha'ban"), "Rewah")
+        self.assertEqual(normalize_hijri_month("Ramadan"), "Puasa")
+        self.assertEqual(normalize_hijri_month("Dzulhijah"), "Rayagung")
 
     def test_apes_is_two_steps_before_jaya(self):
         for naktu in range(7, 21):
